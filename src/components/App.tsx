2b5bcba48cbae3bee6b29cc7cd6b3c6ae3076884
Pawel Zareba <pzareba22@gmail.com>
2021-01-26 01:36:15 +0100

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                Henlo
+            </div>
+        )
+    }
+}
+
+export default App;
