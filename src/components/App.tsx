bed3cbd8fa06ed9ba353beb0f46d735373901f61
Pawel Zareba <pzareba22@gmail.com>
2021-01-28 15:30:18 +0100

    started implementing YouTube API

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import SearchBar from './SearchBar'
+import youtube from '../api/youtube'
 
 class App extends React.Component {
 
-    handleSubmit = (term: string) => {
-        console.log(`app says: ${term}`);
+    handleSubmit = async(term: string) => {
+        const response = await youtube.get('/search', {
+            params: {
+                q: term
+            }
+        });
+        console.log(response.data.items[0]);
+
     };
 
     render() {
