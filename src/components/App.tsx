a63f308076d776ec15925aae01941e15c864b73a
Pawel Zareba <pzareba22@gmail.com>
2021-01-30 02:30:52 +0100

    added response type and setState

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import SearchBar from './SearchBar'
 import youtube from '../api/youtube'
+import Video from '../interfaces/video'
 
-class App extends React.Component {
+interface IState {
+    videos: Array<Video>
+}
+
+class App extends React.Component<{}, IState> {
+    state = {videos: []};
 
     handleSubmit = async(term: string) => {
         const response = await youtube.get('/search', {
@@ -10,8 +16,7 @@ class App extends React.Component {
                 q: term
             }
         });
-        console.log(response.data.items[0]);
-
+        this.setState({ videos: response.data.items });
     };
 
     render() {
