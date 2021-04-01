c65801a2ceb96f2781e005acfa504e62ceba22a9
pzareba22 <pzareba22@gmail.com>
2021-04-01 22:13:50 +0200

    implemented video item

--- src/components/VideoItem.tsx
+++ src/components/VideoItem.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import video from '../interfaces/video';
+import './VideoItem.css'
 
-const VideoItem: React.FC<{video: video}> = ({ video }) => {
+interface IProps {
+    onVideoSelect: (video: video) => void
+    video: video
+}
+
+const VideoItem: React.FC<IProps> = ({ video, onVideoSelect }) => {
 
     return(
-        <div>
-            Video
-            {video.snippet.title}
+        <div className="video-item item" onClick={() => {onVideoSelect(video)}}> 
+            <img className="ui image" src={video.snippet.thumbnails.medium.url}/>
+            <div className="content">
+                <div className="header">
+                    {video.snippet.title}
+                </div>
+            </div>
         </div>
-    )
+    );
 };
 
 export default VideoItem;
