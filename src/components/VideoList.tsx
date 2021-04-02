dbb6541bafa4dc55b1e5324856a33db1a0294663
pzareba22 <pzareba22@gmail.com>
2021-04-02 14:56:09 +0200

    added a callback

--- src/components/VideoList.tsx
+++ src/components/VideoList.tsx
@@ -3,17 +3,18 @@ import video from '../interfaces/video';
 import VideoItem from './VideoItem';
 
 interface IProps {
-    videos: Array<video>
+    videos: Array<video>,
+    onVideoSelect: (video: video) => void
 };
 
-const VideoList: React.FC<IProps> = ({ videos }) => {
+const VideoList: React.FC<IProps> = ({ videos, onVideoSelect }) => {
 
     const renderedList = videos.map((video) => {
-        return <VideoItem key={video.id.videoId} video={video} />
+        return <VideoItem key={video.id.videoId} video={video} onVideoSelect={onVideoSelect} />
     });
 
     return (
-        <div>
+        <div className="ui relaxed divided list">
             {renderedList}
         </div>
     )
