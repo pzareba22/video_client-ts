9e916ade99a9414d08a447ce02580614800ca188
pzareba22 <pzareba22@gmail.com>
2021-04-02 14:55:51 +0200

    added video player

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import SearchBar from './SearchBar'
-import youtube from '../api/youtube'
-import video from '../interfaces/video'
-import VideoList from './VideoList'
+import SearchBar from './SearchBar';
+import youtube from '../api/youtube';
+import video from '../interfaces/video';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 interface IState {
     videos: Array<video>
+    selectedVideo: video | null
 };
 
 class App extends React.Component<{}, IState> {
-    state: IState = {videos: []};
+    state: IState = {videos: [], selectedVideo: null};
 
     handleSubmit = async(term: string) => {
         const response = await youtube.get('/search', {
@@ -20,11 +22,19 @@ class App extends React.Component<{}, IState> {
         this.setState({ videos: response.data.items });
     };
 
+    onVideoSelect = (video: video) => {
+        console.log(video)
+        this.setState({
+            selectedVideo: video
+        });
+    }
+
     render() {
         return (
             <div className="ui container">
                 <SearchBar onSubmit={this.handleSubmit}/>
-                <VideoList videos={this.state.videos}/>
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect}/>
             </div>
         )
     }
---
+++ src/components/VideoDetail.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import video from '../interfaces/video'
+
+interface IProps {
+    video: video | null
+};
+
+const VideoDetail: React.FC<IProps> = ({ video }) => {
+
+    if(video){
+        
+        const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+        
+        return(
+            <div>
+                <div className="ui embed">
+                    <iframe src={videoSrc} frameBorder="0" title={video.snippet.title}></iframe>
+                </div>
+                <div className="ui segment">
+                    <h4 className="ui header">{video.snippet.title}</h4>
+                    <p>{video.snippet.description}</p>
+                </div>
+            </div>
+        );
+    }
+    else{
+        return <div className="ui segment">empty 😩</div>;
+    }
+};
+
+export default VideoDetail;
