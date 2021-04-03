f7252e779b4049da1b1e87adb6b97d6299a75e2a
pzareba22 <pzareba22@gmail.com>
2021-04-03 14:06:10 +0200

    refactoring, added componentDidMount

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,42 +1,59 @@
-import React from 'react';
-import SearchBar from './SearchBar';
-import youtube from '../api/youtube';
-import video from '../interfaces/video';
-import VideoList from './VideoList';
-import VideoDetail from './VideoDetail';
+import React from "react";
+import SearchBar from "./SearchBar";
+import youtube from "../api/youtube";
+import video from "../interfaces/video";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 interface IState {
-    videos: Array<video>
-    selectedVideo: video | null
-};
+    videos: Array<video>;
+    selectedVideo: video | null;
+}
 
 class App extends React.Component<{}, IState> {
-    state: IState = {videos: [], selectedVideo: null};
+    state: IState = { videos: [], selectedVideo: null };
 
-    handleSubmit = async(term: string) => {
-        const response = await youtube.get('/search', {
+    handleSubmit = async (term: string) => {
+        const response = await youtube.get("/search", {
             params: {
-                q: term
-            }
+                q: term,
+            },
         });
-        this.setState({ videos: response.data.items });
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0],
+        });
     };
 
     onVideoSelect = (video: video) => {
-        console.log(video)
         this.setState({
-            selectedVideo: video
+            selectedVideo: video,
         });
+    };
+
+    componentDidMount() {
+        this.handleSubmit("Gang Albanii");
     }
 
     render() {
         return (
             <div className="ui container">
-                <SearchBar onSubmit={this.handleSubmit}/>
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect}/>
+                <SearchBar onSubmit={this.handleSubmit} />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList
+                                videos={this.state.videos}
+                                onVideoSelect={this.onVideoSelect}
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
-        )
+        );
     }
 }
 
